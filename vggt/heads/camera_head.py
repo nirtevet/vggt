560cede7fe52fb39bd17db6243d98c1a091769b5
nirtevet <nirtevet@gmail.com>
2025-10-27 13:00:33 +0200

    modified vggt imports

--- vggt/heads/camera_head.py
+++ vggt/heads/camera_head.py
@@ -11,9 +11,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from vggt.layers import Mlp
-from vggt.layers.block import Block
-from vggt.heads.head_act import activate_pose
+from vggt.vggt.layers import Mlp
+from vggt.vggt.layers.block import Block
+from vggt.vggt.heads.head_act import activate_pose
 
 
 class CameraHead(nn.Module):
